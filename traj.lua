cf53fc58396692dfecb3b9d649aae3d3a542e595
VorTex <65015743+ascaxz@users.noreply.github.com>
2021-04-21 09:39:24 +0700

    Update traj.lua

--- traj.lua
+++ traj.lua
@@ -3,7 +3,8 @@ local gv = Vector3.new(0, 196.2, 0);
 local function solvebiquadratic(a, b, c)
     local p = (b * b - 4 * a * c) ^ 0.5;
     local r0 = (-b * p) / (2 * a);
-    return r0 ^ 0.5;
+    r0 = r0 ^ 0.5;
+    return r0;
 end
 
 local function trajectory(o, t, v)
