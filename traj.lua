a190fa322e0d9a8a3c453ac98bbf244ef89068b6
VorTex <65015743+ascaxz@users.noreply.github.com>
2021-04-22 08:34:18 +0700

    Update traj.lua

--- traj.lua
+++ traj.lua
@@ -1,5 +1,4 @@
 local dot = Vector3.new().Dot;
-local gv = Vector3.new(0, 196.2, 0);
 local function solvebiquadratic(a, b, c)
     local p = (b * b - 4 * a * c) ^ 0.5;
     local r0 = (-b - p) / (2 * a);
@@ -7,13 +6,13 @@ local function solvebiquadratic(a, b, c)
     return r0;
 end
 
-local function solvetrajectory(origin, target, speed)
+local function solvetrajectory(origin, target, accel, speed)
     local dir = target - origin;
-    local coef0 = dot(gv, gv) / 4;
-    local coef1 = dot(gv, dir) - speed * speed;
+    local coef0 = dot(accel, accel) / 4;
+    local coef1 = dot(accel, dir) - speed * speed;
     local const = dot(dir, dir);
     local time = solvebiquadratic(coef0, coef1, const);
-    return (time and gv * time / 2 + dir / time), time;
+    return (time and accel * time / 2 + dir / time), time;
 end
 
 
